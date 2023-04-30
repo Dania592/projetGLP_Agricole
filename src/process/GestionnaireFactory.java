9b8a809559e051c3ecaa4820411740d29f6ae24e
Alicia <aliciaberrouane17@gmail.com>
2023-04-30 14:11:20 +0100

    Génération automatique de référence et résolution bug de positionnement

--- src/process/GestionnaireFactory.java
+++ src/process/GestionnaireFactory.java
@@ -23,68 +23,84 @@ import data.structure.Structure;
 import gui.gestionnaire.keys.Animals;
 import gui.gestionnaire.keys.Encloss;
 import gui.gestionnaire.keys.Engins;
+import gui.gestionnaire.keys.Keys;
 import gui.gestionnaire.keys.Outils;
 import gui.gestionnaire.keys.Structures;
 import gui.gestionnaire.keys.Terrains;
 
 public class GestionnaireFactory {
 	
+	private static int counter = 0;
+	
 	public GestionnaireFactory() {
 	}
 	
 	public static Animal createElement(Animals key, Map map) {
+		String reference = generateReference(key);
 		switch (key) {
 			case VACHE:
-				return new Vache(0, 0, 0, null, null, null, null, map);
+				return new Vache(0, 0, 0, null, null, null, reference, map);
 			case MOUTON:
-				return new Mouton(0, 0, 0, null, null, null, null, map);
+				return new Mouton(0, 0, 0, null, null, null, reference, map);
 			case POULE:
-				return new Poule(0, 0, null, 0, null, null, null, null, map);
+				return new Poule(0, 0, null, 0, null, null, null, reference, map);
 			case CHEVRE:
-				return new Chevre(0, 0, 0, null, null, null, null, map);
+				return new Chevre(0, 0, 0, null, null, null, reference, map);
 			case CHIEN:
-				return new Chien(0, 0, null, 0, null, null, null, null, map);
+				return new Chien(0, 0, null, 0, null, null, null, reference, map);
 			default:
 				return null;
 		}
 	}
 	
 	public static Structure createElement(Structures key, Map map) {
+		String reference = generateReference(key);
 		switch (key) {
 			case ABATTOIRE:
-				return new Abatoire(0, 0, null, map);
+				return new Abatoire(0, 0, reference, map);
 			case ENTREPOT:
-				return new Entrepot(0, 0, null, map);
+				return new Entrepot(0, 0, reference, map);
 			case ETABLE:
-				return new Etable(0, 0, null, map);
+				return new Etable(0, 0, reference, map);
 			case MAISON:
-				return new Maison(0, 0, null, map);
+				return new Maison(0, 0, reference, map);
 			case POULAILLER:
-				return new Poulallier(0, 0, null, map);
+				return new Poulallier(0, 0, reference, map);
 			case SALLE_DE_TRAITE:
-				return new SalleDeTraite(0, 0, null, map);
+				return new SalleDeTraite(0, 0, reference, map);
 			case GARAGE:
-				return new Garage(0, 0, null, map);
+				return new Garage(0, 0, reference, map);
 			case GRANGE:
-				return new Grange(0, 0, null, map);
+				return new Grange(0, 0, reference, map);
 			default:
 				return null;
 		}
 	}
 	
 	public static Terrain createElement(Terrains terrain, Map map) {
-		return new Terrain(null, false, 0, 0, map, null);
+		String reference = generateReference(terrain);
+		return new Terrain(reference, false, 0, 0, map, null);
 	}
 	
 	public static Outil createElement(Outils outil, Map map) {
-		return new Outil(null, false, 0, 0, 0, map, outil);
+		String reference = generateReference(outil);
+		return new Outil(reference, false, 0, 0, 0, map, outil);
 	}
 	
 	public static Engin createElement(Engins engin, Map map) {
-		return new Engin(null, false, 0, 0, 0, map, engin);
+		String reference = generateReference(engin);
+		return new Engin(reference, false, 0, 0, 0, map, engin);
 	}
 	
 	public static Enclos createElement(Encloss enclos, Map map) {
-		return new Enclos(0, 0, null, map);
+		String reference = generateReference(enclos);
+		return new Enclos(0, 0, reference, map);
+	}
+	
+	public static String generateReference(Keys key) {
+		String reference = key.name()+counter;
+		counter++;
+		return reference;
+		
 	}
 }
